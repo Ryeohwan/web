137563950b87106c0afc8194ebe5ea1035cb8fcc
Ryeohwan <ahnrh951019@naver.com>
2022-03-11 18:48:46 +0900

    get & post finish!

--- nodejs_inflearn/server_side_javascript/app.js
+++ nodejs_inflearn/server_side_javascript/app.js
@@ -5,11 +5,59 @@ var express = require('express');
 var app = express();
 // 이런 사회적 약속
 const path = require('path');
+
 app.locals.pretty = true;
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(__dirname + '/public')); // 정적인 파일이 위치할 디렉토리
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
+app.get('/topic/:id', function(req, res){
+  var topics = [
+    'javascript is ...',
+    'nodejs is ...',
+    'express is ...'
+  ];
+  var output = `
+    <a href="/topic?id=0">javascript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br>
+    <a href="/topic?id=2">Express</a><br><br>
+    ${topics[req.params.id]}
+  `
+
+  // 쿼리스트링으로 접근할 땐 req.juery 쓰고 sementic은 req.params로 쓰자.
+  // 그 뒤의 id 는 위의 '/topic/:id' 를 따른 것이다.
+  res.send(output);
+});
+//express가 function 호출하는거다.
+// req 먼저 그 다음 res
+// 쿼리스트링에 따라서 웹 애플리케이션이 다르게 동작한다는 것을 알아야 한다.
+
+app.get('/topic/:id/:mode',function(req, res){
+  res.send(req.params.id+','+req.params.mode)
+});   // 이렇게 하면 /id/mode 로 다른 것을 추가하고싶을 때
+
+
+app.get('/form',function(req,res){
+    res.render('form');
+});
+
+app.get('/from_receiver', function(req, res){
+  var title = req.query.title;
+  var dis = req.query.discription;
+  res.send(title+','+dis);
+});
+// get 방식이다. 기본 get 으로 되어있고 .pug 파일에서 post 로 method를
+// 바꿔줘야 post 방식으로 쓸 수 있다.
+
+app.post('/from_receiver', function(req, res){
+  var title = req.body.title;
+  var dis = req.body.discription;
+  res.send(title+','+dis);
+})
 
 app.get("/template", (req, res) => {
     res.render("temp", {time:Date(), _title:'Pug'});  // 객체를 넣어준다. 이렇게 변수를 사용해줄 수 있다.
