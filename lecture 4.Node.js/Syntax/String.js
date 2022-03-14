3b08a95dfa06b720cae186c371f2c0d4f5d32390
Ryeohwan <ahnrh951019@naver.com>
2022-03-14 20:24:28 +0900

    ~ if youtube

---
+++ lecture 4.Node.js/Syntax/String.js
@@ -0,0 +1,7 @@
+console.log('Hi operation');
+console.log(1+5);
+
+var string = 'Hi operation';
+var len = string.length;
+
+console.log(len);
---
+++ lecture 4.Node.js/Syntax/Variable2.js
@@ -0,0 +1,7 @@
+var name = 'RyeoRyeo';
+var letter = 'Dear ' + name + '\noperation cwal RyeoRyeo \npower overwhelming ' + name + '\nblack sheep wall ' + name + '\nshow me the money '+ name;
+console.log(letter);
+// 중복을 제거하자!
+
+var letter = `Dear ${name} \noperation cwal RyeoRyeo \npower overwhelming ${name} \nblack sheep wall ${name} \nshow me the money ${name}\n${1+3}`;
+console.log(letter);
