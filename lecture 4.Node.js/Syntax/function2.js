f0fcfb9300d3e4542675d3635d0ccc81a09e43e3
Ryeohwan <ahnrh951019@naver.com>
2022-03-21 23:40:27 +0900

    function refactoring

--- lecture 4.Node.js/Syntax/function2.js
+++ lecture 4.Node.js/Syntax/function2.js
@@ -2,7 +2,7 @@ console.log(Math.round(1.6));
 console.log(Math.round(1.4));
 
 function sum(first, second){
-  return first + second;
+  return first + second;  // 을 만나면 함수는 종료된다.
 }
 
 console.log(sum(2,4));
