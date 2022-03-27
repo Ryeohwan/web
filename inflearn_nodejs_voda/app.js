6e24b03ef04351d96dc9dd044943a64355ef9239
Ryeohwan <ahnrh951019@naver.com>
2022-03-27 23:58:13 +0900

    nodejs get

--- inflearn_nodejs_voda/app.js
+++ inflearn_nodejs_voda/app.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.listen(3000,function(){
   console.log('start express server on port 3000');
 });
+
+
 
 for(i= 0; i< 9; i++){
   console.log('hoho');
 }
 
 app.get('/',function(req, res){
-    res.send('hi friend');
-    res.sendFile(__dirname+"public/main.html")
-})
+    res.sendFile(__dirname+"/public/main.html" );
+});
+
+app.get('/main',function(req, res){
+    res.sendFile(__dirname+"/public/main.html" );
+});
