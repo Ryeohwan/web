5685beae236e53ff840979de4a442ad68424a462
Ryeohwan <ahnrh951019@naver.com>
2022-03-28 19:43:35 +0900

    start json

--- inflearn_nodejs_voda/app.js
+++ inflearn_nodejs_voda/app.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-Parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));  // 인코딩은 아스키형태를 다른 형태로 치환해서 보내는 것이다. 처리가 쉽
 
+var cors = require('cors');
+
 app.set('view engine','ejs');
 app.listen(3000,function(){
   console.log('start express server on port 3000');
@@ -26,3 +28,12 @@ app.post('/email_post', function(req, res){
 })
 
 // render 는 응답값을 줄 때 데이터랑 html과 결합된 상태로 클라이언트에 내려주겠다.
+
+app.post('/ajax_send_email', function(req, res){
+  console.log(req.body.email);
+  // check valudation about input value => select db
+  var responseData = {'result' : 'ok', 'email': req.body.email};
+  res.json(responseData);
+});
+
+app.use(cors());
