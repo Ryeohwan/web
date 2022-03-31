f309ab034bc4cd521b985c174b066c53d57c4044
Ryeohwan <ahnrh951019@naver.com>
2022-03-31 20:01:01 +0900

    link with mysql

--- inflearn_nodejs_voda/app.js
+++ inflearn_nodejs_voda/app.js
@@ -6,7 +6,16 @@ var bodyParser = require('body-Parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));  // 인코딩은 아스키형태를 다른 형태로 치환해서 보내는 것이다. 처리가 쉽
 
-var cors = require('cors');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host : 'localhost',
+  port : 3306,
+  user : 'ryeo',
+  password : 'todo',
+  database : 'js_study'
+});
+
+connection.connect();
 
 app.set('view engine','ejs');
 app.listen(3000,function(){
@@ -30,10 +39,18 @@ app.post('/email_post', function(req, res){
 // render 는 응답값을 줄 때 데이터랑 html과 결합된 상태로 클라이언트에 내려주겠다.
 
 app.post('/ajax_send_email', function(req, res){
-  console.log(req.body.email);
-  // check valudation about input value => select db
-  var responseData = {'result' : 'ok', 'email': req.body.email};
-  res.json(responseData);
-});
+  var email = req.body.email;
+  var responseData = {};
 
-app.use(cors());
+  var query = connection.query('select name from user where email="' + email + '"' , function(err, rows){
+    if(err) throw err;
+    if(rows[0]){
+      responseData.result = 'ok';
+      responseData.name = rows[0].name;
+    } else {
+      responseData.result = 'none';
+      responseData.name = " ";
+    }
+    res.json(responseData);
+  })
+});
