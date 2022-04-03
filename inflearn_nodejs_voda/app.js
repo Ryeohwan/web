45c4686278c65f833f4d3eb2870755f570ae5dfc
Ryeohwan <ahnrh951019@naver.com>
2022-04-03 17:47:31 +0900

    why ejs cannot get escape data

--- inflearn_nodejs_voda/app.js
+++ inflearn_nodejs_voda/app.js
@@ -6,12 +6,27 @@ var bodyParser = require('body-Parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));  // 인코딩은 아스키형태를 다른 형태로 치환해서 보내는 것이다. 처리가 쉽다.
 var router = require('./router/index');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var flash = require('connect-flash');  // 메세지를 쉽게 전달해주는 것
+app.use(flash());
 
 app.set('view engine','ejs');
 app.listen(3000,function(){
   console.log('start express server on port 3000');
 });
 
+// express session github
+app.use(session({
+ secret: 'keyboard cat',
+ resave: false,
+ saveUninitialized: true,
+ cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(router);
