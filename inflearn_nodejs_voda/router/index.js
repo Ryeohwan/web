ee942499617492e4df45920099b9381c51f03038
Ryeohwan <ahnrh951019@naver.com>
2022-04-03 22:28:09 +0900

    finish log out

--- inflearn_nodejs_voda/router/index.js
+++ inflearn_nodejs_voda/router/index.js
@@ -6,6 +6,7 @@ var main = require('./main/main');
 var email = require('./email/email');
 var join = require('./join/index');
 var login = require('./login/index');
+var logout = require('./logout/index');
 
 router.get('/',function(req, res){ // 루트로 들어와 //여기로 리다이렉트를 하자
     res.sendFile(path.join(__dirname, "../public/main.html"));
@@ -15,4 +16,6 @@ router.use('/main', main); // 메인에 대한 라우터는 이걸 써라
 router.use('/email', email); // email 로 url 오면 이걸로 처리
 router.use('/join', join);
 router.use('/login', login);
+router.use('/logout', logout);
+
 module.exports = router;
--- inflearn_nodejs_voda/router/main/main.js
+++ inflearn_nodejs_voda/router/main/main.js
@@ -6,6 +6,7 @@ var path = require('path'); // 상대경로를 쓰기 위해서 path 사용
 //     res.sendFile(__dirname+"/public/main.html" );
 // });
 
+// main page 는 login이 될 때만 (즉 세션 정보가 있을때만) 접근이 가능하게 하자.
 router.get('/',function(req, res){ // 루트로 들어와 //여기로 리다이렉트를 하자
     var id = req.user;
     //res.sendFile(path.join(__dirname, "../../public/main.html"));
